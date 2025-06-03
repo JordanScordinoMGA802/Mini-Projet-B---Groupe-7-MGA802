ce5a31e317981bdaaf92f78743ca6947f6b1bac1
bmwi3 <julien.giner@utbm.fr>
2025-06-03 16:26:18 -0400

    Ajout de la fonction qui calcule l'intégrale en utilisant la fonction trapezoid inclut dans numpy

--- trapeze_numpy.py
+++ trapeze_numpy.py
@@ -25,3 +25,25 @@ def integ_trapeze_numpy(a,b,p1,p2,p3,p4,n):
     aire= aires.cumsum()[-1]
     
     return aire
+
+def integ_trapeze_numpy_auto(a,b,p1,p2,p3,p4,n):
+    """
+    Intégration en utilisant la méthode de numpy.
+
+    Paramètres:
+    a (float): premier intervalle.
+    b (float): dernier intervalle.
+    p1, p2, p3, p4 (float): Coefficients de la fonction f(x).
+    n (int): step.
+
+    Return:
+    aire (float): Le résultat de l'intégration.
+    """
+    # Vecteur avec toutes les valeurs de x entre a et b
+    x= np.linspace(a, b, n)
+    # Polynome de la fonction pour tout les x
+    poly=p1 + p2*x + p3*x**2 + p4*x**3
+    # Utilisation de la fonction trapezoid fournie par numpy
+    aire=np.trapezoid(poly, x)
+    
+    return aire
