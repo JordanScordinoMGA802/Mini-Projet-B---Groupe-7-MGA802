c24f47c27fb75fe3ca2a0c2aa10e4eea682a3d90
Joss <jordan.scordino-simondi.1@ens.etsmtl.ca>
2025-06-03 16:28:29 -0400

    fonction rectangle numpy

--- Rectangle_numpy.py
+++ Rectangle_numpy.py
@@ -1,18 +1,16 @@
 import numpy as np
 from fonction import fonction_a_integrer_numpy
 from fonction import demander_polynome
-def integrale_python(f,a,b,n) :
+from time import perf_counter
+def integrale_python_perf(f,a,b,n) :
 
     longueur = (b-a)/n
-    coef = demander_polynome()
     surface = 0
     indice = a + longueur/2
     while indice < b :
-        surface += f(indice, coef)*longueur
+        surface += f(indice)*longueur
         indice += longueur
     return surface
-
-print(integrale_python(fonction_a_integrer_numpy,1, 20, 10 ))
 
 
 
