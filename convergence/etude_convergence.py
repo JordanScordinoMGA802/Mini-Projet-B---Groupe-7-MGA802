b244590eac39454aff0a3e0c1e8316ee36727c79
Ilona <ilona.daumas.1@ens.etsmtl.ca>
2025-06-10 06:56:25 +0200

    Script de convergence des 3 méthodes, il affiche les graphiques de convergence où sur un graphique on a une même méthode mais avec les deux techniques (numpy et python de base)

---
+++ convergence/etude_convergence.py
@@ -0,0 +1,117 @@
+"""
+Ce programme permettra de tracer la convergence de toutes les fonctions
+"""
+
+from matplotlib import pyplot as plt
+
+# On importe les fonctions créées
+# Numpy
+from integration_numerique.numpy.Rectangle_numpy import integrale_numpy_rect
+from integration_numerique.numpy.simpson_numpy import integral_simpson_numpy
+from integration_numerique.numpy.trapeze_numpy import integ_trapeze_numpy
+
+# Python de base
+from integration_numerique.python.simpson_python import simpson_python
+from integration_numerique.python.trapeze_python import trapeze_python
+from integration_numerique.python.rectangle_python import rectangle_python
+
+from fonctions.fonction_perf import polynome_aleatoire_perf
+
+# Initialization: on recupère une fonction f(x) à intégrer
+a = -10
+b = 10
+function_a_tester = polynome_aleatoire_perf()
+print(f"Voici le polynôme que l'on va intégrer pour le test de performance: {function_a_tester}")
+coefficients = function_a_tester.coef
+p1 = coefficients[0]
+p2 = coefficients[1]
+p3 = coefficients[2]
+p4 = coefficients[3]
+
+
+# on fait le choix de comparer la convergence pour les même méthodes
+# rectangle
+# Initialistion des données à tracer
+liste_segment = []
+liste_resultats_rect_numpy = []
+liste_resultats_rect_python = []
+
+for n in range(1,200):
+    liste_segment.append(n)
+    liste_resultats_rect_numpy.append(integrale_numpy_rect(function_a_tester,a,b,n))
+    liste_resultats_rect_python.append(rectangle_python(a,b,p1,p2,p3,p4,n))
+
+plt.rcParams['font.size'] = 12
+plt.rcParams['figure.autolayout'] = True
+plt.rcParams['figure.dpi'] = 100
+
+plt.figure(figsize=(8,5))
+#plt.fill_between(data.index,data['min_temp'],data['max_temp'],alpha=0.5,color='gray')
+plt.plot(liste_segment,liste_resultats_rect_numpy,label='Intégrale rectangulaire Numpy',color='red',linewidth=3.0)
+plt.plot(liste_segment,liste_resultats_rect_python,label='Intégrale rectangulaire python',color='blue',linewidth=3.0)
+plt.title("Convergence de la méthode des rectangles")
+plt.xlim(0,len(liste_segment))
+plt.xlabel("Nombre de segment")
+plt.ylabel("Résultat intégrale")
+plt.grid()
+plt.legend()
+#plt.savefig('../../ressources/convergence.pdf')
+plt.show()
+
+
+# trapèze
+# Initialistion des données à tracer
+liste_segment_trap = []
+liste_resultats_trap_numpy = []
+liste_resultats_trap_python = []
+
+for n in range(1,200):
+    liste_segment_trap.append(n)
+    liste_resultats_trap_numpy.append(integ_trapeze_numpy(a,b,function_a_tester,n))
+    liste_resultats_trap_python.append(trapeze_python(a,b,p1,p2,p3,p4,n))
+
+plt.rcParams['font.size'] = 12
+plt.rcParams['figure.autolayout'] = True
+plt.rcParams['figure.dpi'] = 100
+
+plt.figure(figsize=(8,5))
+#plt.fill_between(data.index,data['min_temp'],data['max_temp'],alpha=0.5,color='gray')
+plt.plot(liste_segment_trap,liste_resultats_trap_numpy,label='Intégrale trapèze Numpy',color='red',linewidth=3.0)
+plt.plot(liste_segment_trap,liste_resultats_trap_python,label='Intégrale trapèze Python',color='blue',linewidth=3.0)
+plt.title("Convergence de la méthode des trapèzes")
+plt.xlim(0,len(liste_segment_trap))
+plt.xlabel("Nombre de segment")
+plt.ylabel("Résultat intégrale")
+plt.grid()
+plt.legend()
+#plt.savefig('../../ressources/convergence.pdf')
+plt.show()
+
+
+# simpson
+# Initialistion des données à tracer
+liste_segment_simp = []
+liste_resultats_simp_numpy = []
+liste_resultats_simp_python = []
+
+for n in range(1,200):
+    liste_segment_simp.append(n)
+    liste_resultats_simp_numpy.append(integral_simpson_numpy(a,b,function_a_tester,n))
+    liste_resultats_simp_python.append(simpson_python(a,b,p1, p2, p3, p4, n))
+
+plt.rcParams['font.size'] = 12
+plt.rcParams['figure.autolayout'] = True
+plt.rcParams['figure.dpi'] = 100
+
+plt.figure(figsize=(8,5))
+#plt.fill_between(data.index,data['min_temp'],data['max_temp'],alpha=0.5,color='gray')
+plt.plot(liste_segment_simp,liste_resultats_simp_numpy,label='Intégrale simpson Numpy',color='red',linewidth=3.0)
+plt.plot(liste_segment_simp,liste_resultats_simp_python,label='Intégrale simpson Python',color='blue',linewidth=3.0)
+plt.title("Convergence de la méthode simpson")
+plt.xlim(0,len(liste_segment_simp))
+plt.xlabel("Nombre de segment")
+plt.ylabel("Résultat intégrale")
+plt.grid()
+plt.legend()
+#plt.savefig('../../ressources/convergence.pdf')
+plt.show()
